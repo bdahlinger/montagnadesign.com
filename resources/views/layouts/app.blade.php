2ccb21163cc1bf19e757e70a920b086e050acc2d
Bill Dahlinger <regs@dahlinger4.com>
2020-10-04 16:43:00 -0700

    initial checkin

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Montagna Design</title>
+    @if( env('APP_ENV') != 'local' && env('APP_ENV') != 'dev' )
+{{--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NCLFP8M');</script>--}}
+        <!-- End Google Tag Manager -->
+    @endif
+
+
+    {{--<link rel="apple-touch-icon" sizes="57x57" href="/images/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/images/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/images/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/images/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/images/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+    <link rel="manifest" href="/images/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+
+    <meta property="og:title" content="{{trans('app.meta.title')}}" />
+    <meta property="og:site_name" content="{{trans('app.meta.title')}}" />
+    <meta property="og:description" content="{{trans('app.meta.description')}}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{env('APP_URL')}}" />
+    <meta property="og:image" content="https://www.gothamknightsgame.com/images/gotham-knights-share.jpg" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="675" />
+    <meta property="og:locale" content="en_US" />
+
+    <meta name="twitter:url" value="{{env('APP_URL')}}" />--}}
+
+
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+
+
+    @stack('css')
+
+
+
+</head>
+<body>
+<div id="app">
+    @if( env('APP_ENV') != 'local' && env('APP_ENV') != 'dev' )
+{{--    <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NCLFP8M"
+                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->--}}
+    @endif
+
+
+    @yield('content')
+
+
+
+</div>
+
+
+<script src="{{ mix('/js/manifest.js') }}"></script>
+<script src="{{ mix('/js/vendor.js') }}"></script>
+<script src="{{ mix('/js/app.js') }}"></script>
+
+@stack('extra-js')
+
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+
+                <h1 class="mt-5">
+
+                    <img src="{{Storage::url('montagna-logo.svg')}}" alt="Montagna Design" class="img-fluid">
+
+
+                </h1>
+
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+@endsection
